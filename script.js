6f0f737b17ebe8c259bfe6814b95c2469de28c5b
david abruzzo <davidabruzzogames@gmail.com>
2022-10-02 14:15:39 +0200

    ajouter des images et finaliser le style ainsi que le contenu du tableau et importer un autre police

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("contact").addEventListener("submit", function(e) {
+    e.preventDefault(); /*pour eviter de partir sur une page vide*/
+
+    alert("formulaire envoyé");
+});
