d58bc632f1840819f0c31651a667118f5d4acf92
david abruzzo <david.abruzzo@gmail.com>
2022-10-03 10:26:31 +0200

    contenu soumis au test ,responsive a retravailler

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementById("contact").addEventListener("submit", function(e) {
+let title = document.querySelector("title").textContent;
+
+if (title === "contact") {
+  verif();
+}
+function verif() {
+  document.getElementById("contact").addEventListener("submit", function (e) {
     e.preventDefault(); /*pour eviter de partir sur une page vide*/
 
     alert("formulaire envoyé");
-});
+  });
+}
